3be7e956f1f6ebe5888f3afa13f616dab4be3b43
Weston Platter <westonplatter@gmail.com>
2014-01-28 15:38:53 -0700

    write tests that prove User and Todo relationship

--- TodoListTests/TodoTests.m
+++ TodoListTests/TodoTests.m
@@ -44,4 +44,37 @@
     XCTAssertTrue(result, @"save operation should be true");
 }
 
+- (void) testRelationshipTodoUser
+{
+//  Create user and todo
+    
+    User *user = [NSEntityDescription insertNewObjectForEntityForName:@"User" inManagedObjectContext:self.moc];
+    [user setEmail:@"todo@example.com"];
+    
+    Todo *todo = [NSEntityDescription insertNewObjectForEntityForName:@"Todo" inManagedObjectContext:self.moc];
+    [todo setAction:@"first"];
+    [todo setUser:user];
+    
+    NSError *error = nil;
+    BOOL result = [self.moc save:&error];
+    
+    XCTAssertNil(error, @"save should not raise errors");
+    XCTAssertTrue(result, @"save should be true");
+
+//  Fetch todo and get user
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *todoEntity = [NSEntityDescription entityForName:@"Todo" inManagedObjectContext:self.moc];
+    [fetchRequest setEntity:todoEntity];
+    NSArray *fetchedObjects = [self.moc executeFetchRequest:fetchRequest error:&error];
+    
+    NSObject *fetchedTodo = [fetchedObjects firstObject];
+    NSObject *fetchedUser = [fetchedTodo valueForKey:@"user"];
+    NSString *email = [fetchedUser valueForKey:@"email"];
+
+//  Check that email is the same as set value
+    
+    XCTAssert([email isEqualToString:@"todo@example.com"], @"user should have email");
+}
+
 @end
--- TodoListTests/UserTests.m
+++ TodoListTests/UserTests.m
@@ -44,5 +44,41 @@
     XCTAssertTrue(result, @"save should be true");
 }
 
+- (void)testRelationshipUserTodos
+{
+    
+//  Create User and Todos
+    
+    User *user = [NSEntityDescription insertNewObjectForEntityForName:@"User" inManagedObjectContext:self.moc];
+    [user setEmail:@"todo@example.com"];
+    
+    Todo *todo_1 = [NSEntityDescription insertNewObjectForEntityForName:@"Todo" inManagedObjectContext:self.moc];
+    [todo_1 setAction:@"first"];
+    [todo_1 setUser:user];
+
+    Todo *todo_2 = [NSEntityDescription insertNewObjectForEntityForName:@"Todo" inManagedObjectContext:self.moc];
+    [todo_2 setAction:@"second"];
+    [todo_2 setUser:user];
+    
+    NSError *error = nil;
+    BOOL result = [self.moc save:&error];
+    
+    XCTAssertNil(error, @"save should not raise errors");
+    XCTAssertTrue(result, @"save should be true");
+
+//  Fetch user and todos
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"User" inManagedObjectContext:self.moc];
+    [fetchRequest setEntity:entity];
+    NSArray *fetchedObjects = [self.moc executeFetchRequest:fetchRequest error:&error];
+
+//  Check that user.todos is array of 2 objects
+    
+    NSObject *fetchedUser = [fetchedObjects firstObject];
+    NSArray *todos = [fetchedUser valueForKey:@"todos"];
+    XCTAssert([todos count] == 2, @"user should have 2 todos");
+}
+
 @end
 
